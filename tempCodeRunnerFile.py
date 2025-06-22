d6860419b10cab7cb7c90a98421423f82aafb4ac
PruthilPatel2209 <pruthil2006@gmail.com>
2025-06-22 11:11:07 +0530

    First commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,36 +1,62 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,session, make_response,redirect
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = 'mysecretkey'
 
 bootstrap = Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///./data.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/', methods=['POST','GET'])
-def hello():
-    # if request.method == 'GET' :
-    #     return "THIS IS A GET REQUEST"
-    # elif request.method == 'POST':
-    #     return "THIS IS A POST REQUEST"
-    text = "This example of Filter"
-    return render_template('index.html', text=text)
+db = SQLAlchemy(app)
 
-@app.template_filter('repeats')
-def repeat(string, n) :
-    return string * n;
+class TODO(db.Model) : 
 
-@app.route('/handle_url') 
-def handle_urls() :
-    if 'greeting' in request.args.keys and 'name' in request.args.keys :
-        greeting = request.args.get('greeting')
-        name = request.args.get('name')
-        return f"{greeting} {name}"
-    else : 
-        return "<h1>YOU HAVE MISSING PARAMETERS</h1>"
+    srno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    desc = db.Column(db.String(255), nullable=False)
+    time = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str :
+       return f'{self.srno} - {self.title}'    
+  
+@app.route('/', methods=['GET', 'POST'])
+def todo():
+
+    if request.method=="POST" :
+        title = request.form.get('title')
+        desc = request.form.get('desc')
+        todo = TODO(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+
+    alltodos = TODO.query.all()
+
+    return render_template('Todo.html', todos=alltodos)
+
+@app.route('/delete/<int:srno>')
+def delete(srno):
+
+    task = TODO.query.filter_by(srno=srno).first()
+    db.session.delete(task)
+    db.session.commit()
+
+    return redirect('/')
+
+@app.route('/update/<int:srno>', methods=['GET', 'POST'])
+def update(srno):
     
-# @app.route('/todo')
-# def index():
+    task = TODO.query.filter_by(srno=srno).first()
+    if request.method == 'POST':
+        task.title = request.form.get('title')
+        task.desc = request.form.get('desc')
+        db.session.commit()
+        return redirect('/')
+    
+    return render_template('update.html', task=task)
 
-#     return render_template('Todo.html')
 
 if __name__ == '__main__':
- app.run(debug=True)
+    app.run(debug=True)
